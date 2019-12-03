d5df0ef8e9b8385f52e731f3b8a4f0aed60cc152
Renjel Cabaral <renjelc@gmail.com>
2019-12-03 12:46:48 +0800

    Initial commit

---
+++ build/precache-manifest.64b103d60b67fc7abbc62c1a1bed3639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ee5a1a0def047ad494500f122813f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "776f620015d537878df7",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5040e8c2a0be59edad8c",
+    "url": "/static/js/2.2523cf63.chunk.js"
+  },
+  {
+    "revision": "776f620015d537878df7",
+    "url": "/static/js/main.b3b99a9c.chunk.js"
+  },
+  {
+    "revision": "58a58b1a649aec3c7fbb",
+    "url": "/static/js/runtime-main.fe011cae.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
