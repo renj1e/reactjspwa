afd5c143fb9d527f5b282695b1bd8e24e8b9f62a
Renjel Cabaral <renjelc@gmail.com>
2019-12-03 13:10:40 +0800

    Updates

---
+++ precache-manifest.61b347dc2ad87decf5c50b257cbe954f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cf8fe66fdd04c329647a0d475684eb",
+    "url": "/reactjspwa/index.html"
+  },
+  {
+    "revision": "83c732a9233526932133",
+    "url": "/reactjspwa/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5040e8c2a0be59edad8c",
+    "url": "/reactjspwa/static/js/2.2523cf63.chunk.js"
+  },
+  {
+    "revision": "83c732a9233526932133",
+    "url": "/reactjspwa/static/js/main.6b5c3498.chunk.js"
+  },
+  {
+    "revision": "76b66e6906c97eb02fe8",
+    "url": "/reactjspwa/static/js/runtime-main.6512a14b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactjspwa/static/media/logo.25bf045c.svg"
+  }
+]);
