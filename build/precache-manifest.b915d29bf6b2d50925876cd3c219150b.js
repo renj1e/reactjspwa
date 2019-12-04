6624249afcce128fa86b048bc55b80e500a62926
Renjel Cabaral <renjelc@gmail.com>
2019-12-04 15:31:48 +0800

    meta tags

--- build/precache-manifest.b915d29bf6b2d50925876cd3c219150b.js
+++ build/precache-manifest.b915d29bf6b2d50925876cd3c219150b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4a84b369b11f46fbbc2c3063adb48cd1",
+    "revision": "6174ed34d767f2275ed4589a3d87593a",
     "url": "/reactjspwa/index.html"
   },
   {
-    "revision": "ab601103ad1ac44d91c2",
+    "revision": "d23ed8fd732dcb7c4af9",
     "url": "/reactjspwa/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactjspwa/static/js/2.2523cf63.chunk.js"
   },
   {
-    "revision": "ab601103ad1ac44d91c2",
-    "url": "/reactjspwa/static/js/main.ec2c9611.chunk.js"
+    "revision": "d23ed8fd732dcb7c4af9",
+    "url": "/reactjspwa/static/js/main.0feb2eee.chunk.js"
   },
   {
     "revision": "76b66e6906c97eb02fe8",
