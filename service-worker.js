19bb1e09bfaaef39b60c6bd1ea740807b39b2daa
Renjel Cabaral <renjelc@gmail.com>
2019-12-04 17:13:42 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactjspwa/precache-manifest.d0f8ef74b272ea2feef690dfbb0a8106.js"
+  "/reactjspwa/precache-manifest.06e12431844e233401dba39e8164327c.js"
 );
 
 self.addEventListener('message', (event) => {
